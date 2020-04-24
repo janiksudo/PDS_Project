af1cfddfa2104483e6a69095b3bf8e568bcbfbaf
Janik Muires <2056743+janiksudo@users.noreply.github.com>
2020-04-24 10:30:52 +0200

    requirements.txt and example notebook extended setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='PDS_Project',
     version='0.0.1dev1',
     description="Semester Project - Programming Data Science",
-    author="Student",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="",
     author_email="student@uni-koeln.de",
-    packages=["nextbike"],
+    url="https://github.com/janiksudo/PDS_Project",
+    packages=setuptools.find_packages(),
     install_requires=['pandas', 'scikit-learn', 'click'],
     entry_points={
         'console_scripts': ['nextbike=nextbike.cli:main']
