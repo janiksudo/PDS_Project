5e8d22e15a5dca0e72f9499fd3a6b04793280853
Janik Muires <2056743+janiksudo@users.noreply.github.com>
2020-05-06 11:15:24 +0200

    test Preprocessor class with notebook

--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -7,4 +7,4 @@ def save_model(model):
     pickle.dump(model, open(os.path.join(get_data_path(), "output/model.pkl"), 'wb'))
 
 def save_df(df, name):
-    df.to_csv(os.path.join(get_data_path(), '/processed/', name, '.csv'), index=False)
+    df.to_csv(os.path.join(get_data_path(), 'processed/' + name + '.csv'), index=False)
