84ee72d6be5c1ccfbbfc16e9d4e46f1a20d018e5
Janik Muires <2056743+janiksudo@users.noreply.github.com>
2020-06-02 21:04:13 +0200

    Preprocessor finished, but untested

--- nextbike/preprocessing/Preprocessor.py
+++ nextbike/preprocessing/Preprocessor.py
@@ -1,13 +1,14 @@
 from .. import io
 import datetime
 import geopandas as gpd
+import json
 import os
 import pandas as pd
 from pandas.io.json import json_normalize
+import requests
 import seaborn as sns
 from shapely.ops import cascaded_union
 from tqdm import tqdm
-import json
 
 
 class Preprocessor:
@@ -194,7 +195,206 @@ class Preprocessor:
 
         print(len(self._trips), 'trips remaining.')
 
+        # finally fill na values with zeros
+        self._trips.fillna(0, inplace=True)
+
         # Save trips data set as csv in data/preprocessed.
         io.save_df(self._trips, 'trips')
         print('Trips data set saved in data/processed as trips.csv.')
         print('Creating trips from data completed successfully.')
+
+    def _get_trips(self):
+        return io.read_file(path=os.path.join(self._datapath, 'processed/trips.csv'))
+
+    def _saveDwdData(self, url, path, f_name):
+
+        print('Download ' + f_name + ' from ' + url + ' and save to ' + path)
+
+        file = requests.get(url)
+        open(path + f_name, 'wb').write(file.content)
+        file.close()
+
+    def _getDwdData(self, url, path, f_name):
+
+        self._saveDwdData(url, path, f_name)
+
+        print()
+
+        df = pd.read_csv(path + f_name, sep=';')
+        print('Created data frame of ' + f_name)
+
+        os.remove(path + f_name)
+        print('Zip file removed: ' + path + f_name)
+
+        return df
+
+    def _filterForYear(self, df, year):
+
+        df.rename(columns={"MESS_DATUM": "timestamp"}, inplace=True)
+
+        df['timestamp'] = pd.to_datetime(df['timestamp'].astype(str))
+
+        df.set_index('timestamp', inplace=True)
+
+        df = df[df.index.year == year]
+
+        return df
+
+    def prepWeather(self):
+        path = os.path.join(self._datapath, 'external/')
+
+        urls = {
+            'air_temp': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/air_temperature/historical/10minutenwerte_TU_00691_20100101_20191231_hist.zip',
+            'air_temp_extr': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/extreme_temperature/historical/10minutenwerte_extrema_temp_00691_20100101_20191231_hist.zip',
+            'wind': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/wind/historical/10minutenwerte_wind_00691_20100101_20191231_hist.zip',
+            'wind_extr': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/extreme_wind/historical/10minutenwerte_extrema_wind_00691_20100101_20191231_hist.zip',
+            'precipitation': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical/10minutenwerte_nieder_00691_20100101_20191231_hist.zip',
+            'solar': 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/solar/historical/10minutenwerte_SOLAR_00691_20100101_20191231_hist.zip'
+        }
+
+        air_temp = self._getDwdData(urls['air_temp'], path, 'air_temp.zip')
+
+        air_temp_extr = self._getDwdData(
+            urls['air_temp_extr'], path, 'air_temp_extr.zip')
+
+        wind = self._getDwdData(urls['wind'], path, 'wind.zip')
+
+        wind_extr = self._getDwdData(urls['wind_extr'], path, 'wind_extr.zip')
+
+        precipitation = self._getDwdData(
+            urls['precipitation'], path, 'precipitation.zip')
+
+        solar = self._getDwdData(urls['solar'], path, 'solar.zip')
+        print('Cleaning air_temp')
+
+        air_temp = self._filterForYear(air_temp, 2019)
+
+        # Drop unnecessary columns
+        air_temp.drop(columns={'STATIONS_ID', '  QN',
+                               'PP_10', 'eor'}, inplace=True)
+
+        # Assign interpretable column names
+        air_temp.rename(columns={"TT_10": "temp_2m", "TM5_10": "temp_5cm",
+                                 "RF_10": "humidity_2m", "TD_10": "dew_point_2m"}, inplace=True)
+        air_temp.drop(columns={'temp_5cm'}, inplace=True)
+
+        air_temp.replace(-999, float('NaN'), inplace=True)
+        air_temp.dropna(inplace=True)
+
+        print('Cleaning air_temp_extr')
+
+        air_temp_extr = self._filterForYear(air_temp_extr, 2019)
+
+        # Drop unnecessary columns
+        air_temp_extr.drop(
+            columns=['STATIONS_ID', '  QN', 'eor'], inplace=True)
+
+        # Assign interpretable column names
+
+        air_temp_extr.rename(columns={'TX_10': 'max_at_2m', 'TX5_10': 'max_at_5cm',
+                                      'TN_10': 'min_at_2m', 'TN5_10': 'min_at_5cm'}, inplace=True)
+        air_temp_extr.drop(columns={'min_at_2m', 'min_at_5cm'}, inplace=True)
+        air_temp_extr.drop(columns={'max_at_5cm'}, inplace=True)
+
+        air_temp_extr.replace(-999, float('NaN'), inplace=True)
+        air_temp_extr.dropna(inplace=True)
+
+        print('Cleaning wind')
+
+        wind = self._filterForYear(wind, 2019)
+
+        # Drop unnecessary columns
+        wind.drop(columns={'STATIONS_ID', '  QN', 'eor'}, inplace=True)
+
+        # Assign interpretable column names
+        wind.rename(columns={'FF_10': 'mean_speed_h/s',
+                             'DD_10': 'direction_degree'}, inplace=True)
+
+        wind.replace(-999, float('NaN'), inplace=True)
+        wind.dropna(inplace=True)
+
+        print('Cleaning wind_extr')
+
+        wind_extr = self._filterForYear(wind_extr, 2019)
+
+        # Drop unnecessary columns
+        wind_extr.drop(columns={'STATIONS_ID', '  QN', 'eor'}, inplace=True)
+
+        # Assign interpretable column names
+        wind_extr.rename(columns={'FX_10': 'max_m/s', 'FNX_10': 'min_mean_m/s',
+                                  'FMX_10': 'max_mean_m/s', 'DX_10': 'direction_degree'}, inplace=True)
+
+        wind_extr.replace(-999, float('NaN'), inplace=True)
+        wind_extr.dropna(inplace=True)
+
+        print('Cleaning precipitation')
+
+        precipitation = self._filterForYear(precipitation, 2019)
+
+        # Drop unnecessary columns
+        precipitation.drop(
+            columns={'STATIONS_ID', '  QN', 'eor', 'RWS_IND_10'}, inplace=True)
+
+        # Assign interpretable column names
+        precipitation.rename(
+            columns={'RWS_DAU_10': 'min', 'RWS_10': 'mm'}, inplace=True)
+
+        precipitation.replace(-999, float('NaN'), inplace=True)
+        precipitation.dropna(inplace=True)
+
+        print('Cleaning solar')
+
+        solar = self._filterForYear(solar, 2019)
+
+        # Drop unnecessary columns
+        solar.drop(columns={'STATIONS_ID', '  QN', 'eor'}, inplace=True)
+
+        # Assign interpretable column names
+        solar.rename(columns={'DS_10': 'diffuse_radiation', 'GS_10': 'incoming_radiation',
+                              'SD_10': 'duration_h', 'LS_10': 'longwave_downward_radiation'}, inplace=True)
+
+        solar.drop(columns={'longwave_downward_radiation'}, inplace=True)
+
+        solar.replace(-999, float('NaN'), inplace=True)
+        solar.dropna(inplace=True)
+
+        print('Merging all weather data...')
+
+        all_weather = pd.merge(air_temp, air_temp_extr, on='timestamp')
+
+        all_weather = pd.merge(all_weather, wind, on='timestamp')
+
+        all_weather = pd.merge(all_weather, wind_extr, on='timestamp')
+
+        all_weather = pd.merge(all_weather, precipitation, on='timestamp')
+
+        print(all_weather.where(all_weather == -999).count())
+
+        all_weather.to_csv(path + 'weather.gz', compression='gzip')
+
+        print('Getting and cleaning of weather data successful!')
+        print('Data saved as ' + path + 'weather.gz')
+        print('To import the data use the following command:')
+        print("pd.read_csv(path + 'weather.gz', index_col='timestamp')")
+
+    def mergeWeatherTrips(self, trips, weather):
+        weather['timestamp'] = pd.to_datetime(weather['timestamp'])
+
+        # Floor start time to next 10 minutes so we can merge it with weather data
+
+        trips['sTime_floored'] = pd.to_datetime(
+            trips['start_time']).dt.floor('10T')
+        trips['sTime_floored'] = pd.to_datetime(trips['sTime_floored'])
+        data = trips.merge(right=weather, left_on='sTime_floored',
+                           right_on='timestamp', how='left')
+        data.drop(columns=['sTime_floored'], inplace=True)
+        print("Trip and weather data merged")
+        print("Clean data")
+        data.drop(columns=["bike_type", "mm"], inplace=True)
+        data.dropna(inplace=True)
+
+        # save to /data/processed/trips_weather.zip
+        io.save_df(data, 'trips_weather')
+
+    def _get_merged(self):
+        return io.read_file(path=os.path.join(self._datapath, 'processed/trips_weather.csv'))
