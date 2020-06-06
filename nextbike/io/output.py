08c1d3942038a3b5eb31a0376828c92ad47d3293
tcelig <tcelig@smail.uni-koeln.de>
2020-06-06 12:29:28 +0200

    #24 refactored trips_weather to dataset

--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -1,10 +1,11 @@
-from .utils import get_data_path
+from .utils import *
 import os
 import pickle
 
 
 def save_model(model, modelName):
-    pickle.dump(model, open(os.path.join(get_data_path(), "output/" + modelName + ".pkl + "), 'wb'))
+    with open(os.path.join(get_model_path(), '' + modelName + '.pkl'), 'wb') as pickle_file:
+        pickle.dump(model, pickle_file)
 
 def save_df(df, name):
     df.to_csv(os.path.join(get_data_path(), 'processed/' + name + '.csv'), index=False)
