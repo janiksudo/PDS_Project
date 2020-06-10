b463ce036d3082c342db5e107f9979552b448178
Janik M <2056743+janiksudo@users.noreply.github.com>
2020-06-10 10:45:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PDS_Project',
-    version='1.0.0-rc.1',
+    version='1.0.0',
     description="Semester Project - Programming Data Science",
     long_description=long_description,
     long_description_content_type="text/markdown",
